b544c24eaf82a5d0619e464866544129d77c6c3b
Yassine Khalfallah <69521115+FliXnfm@users.noreply.github.com>
2024-11-14 09:40:09 +0000

    Initialisation du projet avec ajout de docstrings et correction des erreurs pylint

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
+"""
+Module pour vérifier les nombres premiers et les lister jusqu'à une limite donnée.
+"""
+
 from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
+    """Vérifie si un nombre est premier.
 
-    # votre code ici
+    Args:
+        p (int): Le nombre à vérifier.
 
-    pass
+    Returns:
+        bool: True si p est un nombre premier, False sinon.
+    """
+    if p <= 1:
+        return False
+    if p == 2:
+        return True
+    if p % 2 == 0:
+        return False
+    for i in range(3, int(sqrt(p)) + 1, 2):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """Fonction principale pour lister les nombres premiers jusqu'à 100."""
+    # Appels à la fonction secondaire
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
 
 if __name__ == "__main__":
     main()
